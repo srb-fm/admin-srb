aa02ecbdff8ada6214f6a6e479f28395fbdc4d57
js workshop <js@ggggoog.com>
2013-04-08 11:00:06 +0200

    code cleanig

--- tools/watch_dog_mp3_gain.py
+++ tools/watch_dog_mp3_gain.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable-msg=C0103
 
 """
 Watch Dog mp3gain
@@ -43,10 +44,8 @@ William Somerset Maugham
 import sys
 import os
 import string
-import types
 import shutil
 import subprocess
-import socket
 import lib_common_1 as lib_cm
 
 
@@ -65,9 +64,12 @@ class app_config( object ):
         self.app_errorfile = "error_watch_dog_mp3gain.log"
         # errorlist
         self.app_errorslist = []
-        self.app_errorslist.append(u"Error 000 Parameter-Typ oder Inhalt stimmt nicht ")
-        self.app_errorslist.append(u"Error 001 bei mp3gain:")
-        self.app_errorslist.append(u"Error 002 Fehler beim Kopieren der bearbeiteten Datei ")
+        self.app_errorslist.append(u"Error 000 "
+            "Parameter-Typ oder Inhalt stimmt nicht ")
+        self.app_errorslist.append(u"Error 001 "
+            "bei mp3gain:")
+        self.app_errorslist.append(u"Error 002 "
+            "Fehler beim Kopieren der bearbeiteten Datei ")
         # params-type-list, typ entsprechend der params-liste in der config
         self.app_params_type_list = []
         self.app_params_type_list.append("p_string")
@@ -82,39 +84,43 @@ class app_config( object ):
         self.app_windows = "no"
         self.app_encode_out_strings = "cp1252"
 
-def audio_mp3gain(ac, path_file):
+
+def audio_mp3gain(path_file):
     """mp3-File Gainanpassung"""
-    lib_cm.message_write_to_console( ac, u"mp3-File Gainanpassung" )
-    #damit die uebergabe der befehle richtig klappt muessen alle cmds im richtigen zeichensatz encoded sein
-    c_mp3gain = db.ac_config_1[1].encode( ac.app_encode_out_strings )
-    lib_cm.message_write_to_console( ac, c_mp3gain )
-    c_source_file = path_file.encode( ac.app_encode_out_strings )
-    lib_cm.message_write_to_console( ac, c_source_file )
+    lib_cm.message_write_to_console(ac, u"mp3-File Gainanpassung" )
+    # damit die uebergabe der befehle richtig klappt,
+    # muessen alle cmds im richtigen zeichensatz encoded sein
+    c_mp3gain = db.ac_config_1[1].encode(ac.app_encode_out_strings )
+    lib_cm.message_write_to_console(ac, c_mp3gain )
+    c_source_file = path_file.encode(ac.app_encode_out_strings )
+    lib_cm.message_write_to_console(ac, c_source_file )
     # subprozess starten
     try:
-        p = subprocess.Popen([c_mp3gain, u"-r", c_source_file],  stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate(  )
+        p = subprocess.Popen([c_mp3gain, u"-r", c_source_file], 
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     except Exception, e:
         log_message = ac.app_errorslist[1] + u": %s" % str(e)
         db.write_log_to_db_a(ac, log_message, "x", "write_also_to_console" )
         return 
 
-    lib_cm.message_write_to_console( ac, u"returncode 0" )
-    lib_cm.message_write_to_console( ac, p[0] )
-    lib_cm.message_write_to_console( ac, u"returncode 1" )
-    lib_cm.message_write_to_console( ac, p[1] )
+    lib_cm.message_write_to_console(ac, u"returncode 0" )
+    lib_cm.message_write_to_console(ac, p[0] )
+    lib_cm.message_write_to_console(ac, u"returncode 1" )
+    lib_cm.message_write_to_console(ac, p[1] )
     
     # erfolgsmeldung suchen, wenn nicht gefunden: -1
-    mp3gain_output = string.find( p[1],  "99%" )
-    mp3gain_output_1 = string.find( p[1],  "written" )
-    lib_cm.message_write_to_console( ac, mp3gain_output )
-    lib_cm.message_write_to_console( ac, mp3gain_output_1 )
+    mp3gain_output = string.find( p[1], "99%" )
+    mp3gain_output_1 = string.find( p[1], "written" )
+    lib_cm.message_write_to_console(ac, mp3gain_output )
+    lib_cm.message_write_to_console(ac, mp3gain_output_1 )
     # wenn gefunden, position, sonst -1
     if mp3gain_output != -1 and mp3gain_output_1 != -1:
         log_message = u"mp3gain angepasst: " + c_source_file
-        db.write_log_to_db( ac, log_message, "k" )
-        lib_cm.message_write_to_console( ac, "ok" )
+        db.write_log_to_db(ac, log_message, "k" )
+        lib_cm.message_write_to_console(ac, "ok" )
     else:
-        db.write_log_to_db_a(ac,  u"mp3gain offenbar nicht noetig: " + c_source_file, "p", "write_also_to_console" )
+        db.write_log_to_db_a(ac, u"mp3gain offenbar nicht noetig: " 
+                             + c_source_file, "p", "write_also_to_console")
 
 def lets_rock():
     """Hauptfunktion """
@@ -129,10 +135,10 @@ def lets_rock():
     
     # mp3gain-Ordner einlesen
     try:
-        files_source = os.listdir( path_source )
+        files_source = os.listdir(path_source )
     except Exception, e:
         log_message = u"read_files_from_dir Error: %s" % str(e)
-        lib_cm.message_write_to_console( ac, log_message + path_source )
+        lib_cm.message_write_to_console(ac, log_message + path_source)
         db.write_log_to_db( ac, log_message, "x" )
         return None
     
@@ -144,16 +150,18 @@ def lets_rock():
             continue
         z += 1
         path_file_source = path_source + item
-        audio_mp3gain(ac, path_file_source )
+        # audio_mp3gain(ac, path_file_source )
+        audio_mp3gain(path_file_source )
             
         # verschieben
         path_file_dest = path_dest + item
         try:
-            shutil.move( path_file_source, path_file_dest )
+            shutil.move(path_file_source, path_file_dest )
         except Exception, e:
-            db.write_log_to_db_a(ac,  ac.app_errorslist[2], "x", "write_also_to_console" )
+            db.write_log_to_db_a(ac, ac.app_errorslist[2], 
+                "x", "write_also_to_console" )
             log_message = u"copy_files_to_dir_retry Error: %s" % str(e)
-            lib_cm.message_write_to_console( ac, log_message )
+            lib_cm.message_write_to_console(ac, log_message )
             db.write_log_to_db(ac, log_message, "x" )
             continue        
         
@@ -163,10 +171,8 @@ def lets_rock():
         else:
             filename = path_file_dest[string.rfind(path_file_dest,  "\\")+1:]
             
-        #db.write_log_to_db_a(ac, u"Audio mit mp3gain bearbeitet und kopiert: " + filename, "c", "write_also_to_console" )
-        db.write_log_to_db_a(ac, u"Audio mit mp3gain bearbeitet und kopiert: " + filename, "i", "write_also_to_console" )
-    #if z == 0:
-        #lib_cm.message_write_to_console(ac, u"Keine Audios zum Bearbeiten vorhanden")
+        db.write_log_to_db_a(ac, u"Audio mit mp3gain bearbeitet und kopiert: " 
+            + filename, "i", "write_also_to_console" )
 
 
 if __name__ == "__main__":
@@ -174,7 +180,7 @@ if __name__ == "__main__":
     ac = app_config()
     print  "lets_work: " + ac.app_desc 
     # losgehts
-    #db.write_log_to_db(ac,  ac.app_desc + u" gestartet", "a")
+    # db.write_log_to_db(ac,  ac.app_desc + u" gestartet", "a")
     # Config_Params 1
     db.ac_config_1 = db.params_load_1(ac,  db)
     if db.ac_config_1 is not None:
@@ -184,6 +190,6 @@ if __name__ == "__main__":
             lets_rock()
 
     # fertsch
-    #db.write_log_to_db(ac,  ac.app_desc + u" gestoppt", "s")
+    # db.write_log_to_db(ac,  ac.app_desc + u" gestoppt", "s")
     print "lets_lay_down"
     sys.exit()
