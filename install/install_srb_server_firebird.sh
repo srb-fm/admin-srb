eafd5194fc852f1bfd672c6f5892ebf5eebeba41
Joerg Sorge <technik@srb.fm>
2015-03-13 14:18:01 +0100

    changed desc., remove flamerobin

--- install/install_srb_server_firebird.sh
+++ install/install_srb_server_firebird.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Dieses kleine Script uebernimmt die Instalaltion 
+# This script is for installing server-packages.
+# Some of them must be configured later.
+# Testet with ubuntu-server 14.04 in 2015
+#
+# Dieses kleine Script uebernimmt die Installation 
 # des Firebird-Servers Admin-SRB auf dem Server
 # Bei einigen ist eine spaetere Configuration noetig
 # Author: Joerg Sorge
@@ -8,7 +12,7 @@
 # Copyright (C) Joerg Sorge joergsorge@gmail.com
 # 2013-05-27
 #
-# ToDo 
+
 
 echo "SRB-Admin-Firebird Installation..."
 
@@ -16,19 +20,19 @@ sudo apt-get install \
 firebird2.5-super firebird2.5-examples firebird2.5-dev \
 flamerobin php5-interbase python-kinterbasdb
 
-echo "Konfiguration"
+echo "Configuration"
 sudo dpkg-reconfigure firebird2.5-super
 
-echo "Backup-Verzeichnis einrichten"
+echo "SetUp Backup-Path"
 sudo mkdir ~/srb-firebird-backup
 sudo chown -R firebird:firebird ~/srb-firebird-backup
 
 
-echo "Alias anpassen.."
+echo "Customize Alias ..."
 sudo cp /etc/firebird/2.5/aliases.conf /etc/firebird/2.5/aliases.conf.$(date +'%y-%m-%d-%H-%M-%S')
 sudo bash -c "echo ""Admin_SRB_db = /var/lib/firebird/2.5/data/admin_srb_db.fdb"" >> /etc/firebird/2.5/aliases.conf"
 sudo bash -c "echo ""Admin_SRB_db_log = /var/lib/firebird/2.5/data/admin_srb_db_log.fdb"" >> /etc/firebird/2.5/aliases.conf"
 
-echo "...abgeschlossen"
-read -p "Press Enter to finish..."
+echo "...finish"
+read -p "Press Enter to close..."
 exit
