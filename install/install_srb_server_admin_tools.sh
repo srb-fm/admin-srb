e063f313305ec52caa44bf1596205cb237c6e38a
Joerg Sorge <technik@srb.fm>
2015-03-16 15:12:31 +0100

    some mods

--- install/install_srb_server_admin_tools.sh
+++ install/install_srb_server_admin_tools.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is for installing server-packages.
+# Some of them must be configured later.
+# Testet with ubuntu-server 14.04 in 2015
+#
 # Dieses kleine Script uebernimmt die Instalaltion 
 # der SRB-Tools fuer Admin-SRB auf dem Server
 # Bei einigen ist eine spaetere Configuration noetig
@@ -11,43 +15,37 @@
 
 
 echo "SRB-Admin-Tools Installation..."
-echo "SRB-Admin von git laden..."
+echo "Load Admin-SRB from git..."
 
 git clone https://github.com/srb-fm/admin-srb.git
 
-echo "Verzeichnis einrichten"
-echo "~/srb-tools"
+echo "Make Path ~/srb-tools"
 mkdir ~/srb-tools
 
-echo "Tools kopieren"
-cp ~/admin-srb/tools/*.py ~/srb-tools
-echo "Tools ausfuehrbar machen"
-cd ~/srb-tools
-chmod u+x *
+echo "Copy Tools"
+cp "$(pwd)"/admin-srb/tools/*.py ~/srb-tools
+echo "Make Tools executable"
+chmod u+x ~/srb-tools/*.py
 
-echo "Intra kopieren" 
-sudo cp -R ~/admin-srb/intra/* /var/www
+echo "Make Path ~/srb-intra"
+sudo mkdir /var/www/srb-intra
+echo "Change User:Group for srb-intra"
+sudo chown $USER:www-data /var/www/srb-intra
+echo "Make symbolic Link from /var/www/srb-intra to home"
+ln -s /var/www/srb-intra ~/
+mkdir ~/srb-intra/public_html
+mkdir ~/srb-intra/cgi-bin
+mkdir ~/srb-intra/cgi-bin/admin_srb_libs
+echo "Make Export-Path"
+sudo mkdir ~/srb-intra/public_html/admin_srb_export
+echo "Change User:Group for srb-export"
+sudo chown -R www-data:www-data ~/srb-intra/public_html/admin_srb_export
+echo "Copy Intra" 
+sudo cp -R "$(pwd)"/admin-srb/intra/* ~/srb-intra/public_html
+sudo cp -R "$(pwd)"/admin-srb/intra/admin_srb_libs/ ~/srb-intra/cgi-bin/
 
-echo "Gruppe aendern"
-sudo groupadd www
-sudo chgrp -R www /var/www
-sudo usermod -aG users www-data
+echo "Folder admin-srb will continue stay for later use of install-scripts..."
 
-echo "Export-Verzeichnis einrichten"
-sudo mkdir /var/www/admin_srb_export
-sudo chown -R www-data:www-data /var/www/admin_srb_export
-
-
-echo "Libs verschieben"
-sudo mkdir /var/cgi-bin
-sudo mv /var/www/admin_srb_libs /var/cgi-bin/admin_srb_libs
-
-echo "Admin-SRB aufraeumen"
-rm -rf ~/admin-srb 
-
-
-
-
-echo "...abgeschlossen"
-read -p "Press Enter to finish..."
+echo "...finish"
+read -p "Press Enter to close..."
 exit
