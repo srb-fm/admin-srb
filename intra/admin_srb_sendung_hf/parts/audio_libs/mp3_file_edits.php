fa2d5169a5dc1a6c5ed166abbb0de182ab152c7b
Joerg Sorge <joergsorge@gmail.com>
2015-05-04 17:30:42 +0200

    correction of preserve ape-tags

--- intra/admin_srb_sendung_hf/parts/audio_libs/mp3_file_edits.php
+++ intra/admin_srb_sendung_hf/parts/audio_libs/mp3_file_edits.php
@@ -13,6 +13,7 @@
 
 
 
+require_once "../../cgi-bin/admin_srb_libs/lib.php";
 // include getID3() library (can be in a different directory if full path is specified)
 require_once('../parts/get_id3/getid3/getid3.php');
 
@@ -31,69 +32,88 @@ if ( $fp_remote = fopen( $remotefilename, 'rb')) {
         fclose($fp_local);
 	}
 }
-   
-   fclose( $fp_remote );
 
-	// get length
-	// Initialize getID3 engine
-	$getID3 = new getID3;
-	$ThisFileInfo = $getID3->analyze( $localtempfilename );
+fclose( $fp_remote );
 
-	// compare
-	$need_change_id3 = "no";
-	if ( get_time_in_hms ( @$ThisFileInfo['playtime_seconds'] ) != $audio_length ){ $need_change_id3 = "yes";  $log_message .= $need_change_id3." playtime \n". $audio_length." - ".get_time_in_hms ( @$ThisFileInfo['playtime_seconds'])."\n";}
-	if ( @$ThisFileInfo['tags']['id3v2']['title'][0] != $title ){ $need_change_id3 = "yes"; $log_message .= $need_change_id3." title\n";}
-	if ( @$ThisFileInfo['tags']['id3v2']['artist'][0] != $artist ){ $need_change_id3 = "yes"; $log_message .= $need_change_id3." artist\n";}
-	
-	$log_message .= $need_change_id3."\n";
-		
-	if ( $need_change_id3 = "yes" ){		
-		// write tag
-		//$TaggingFormat = 'UTF-8';
-		$TaggingFormat = 'ISO-8859-1';
-		$getID3->setOption(array('encoding'=>$TaggingFormat));
-		// Initialize getID3 tag-writing module
-		require_once('../parts/get_id3/getid3/write.php');
-		$tagwriter = new getid3_writetags;
-		$tagwriter->filename       = $localtempfilename;
-		$tagwriter->tagformats     = array('id3v1', 'id3v2.3', 'ape');
-		// set various options (optional)
-		$tagwriter->overwrite_tags = true;
-		$tagwriter->tag_encoding   = $TaggingFormat;
-		//$tagwriter->remove_other_tags = true;
+// Initialize getID3 engine
+$getID3 = new getID3;
+$ThisFileInfo = $getID3->analyze( $localtempfilename );
 
-		// populate data array
-		$TagData['title'][]   = $title;
-		$TagData['artist'][]  = $artist;
-		$TagData['album'][]   = 'SRB - Das Buergerradio';
-		$TagData['year'][]    = date("Y");
+// compare
+$need_change_id3 = "no";
+if ( get_time_in_hms ( @$ThisFileInfo['playtime_seconds'] ) != $audio_length ) { 
+	$need_change_id3 = "yes";  
+	$log_message .= $need_change_id3." playtime \n". $audio_length." - ".get_time_in_hms ( @$ThisFileInfo['playtime_seconds'])."\n";
+}
+if ( @$ThisFileInfo['tags']['id3v2']['title'][0] != $title ) { 
+	$need_change_id3 = "yes"; 
+	$log_message .= $need_change_id3." title\n";
+}
+if ( @$ThisFileInfo['tags']['id3v2']['artist'][0] != $artist ) { 
+	$need_change_id3 = "yes"; 
+	$log_message .= $need_change_id3." artist\n";
+}
 
-		
-		// save ape-tags for new writing: mp3gain
-		//$ReplayGainTagsToPreserve = array('mp3gain_minmax', 'mp3gain_album_minmax', 'mp3gain_undo', 'replaygain_track_peak', 'replaygain_track_gain', 'replaygain_album_peak', 'replaygain_album_gain');
-		//foreach ($ReplayGainTagsToPreserve as $rg_key) {
-		//	if (isset($ThisFileInfo['ape']['items'][strtolower($rg_key)]['data'][0]) ) {
-				//$TagData[strtoupper($rg_key)][] = $ThisFileInfo['ape']['items'][strtolower($rg_key)]['data'][0];
-			//}
-		//}
+$log_message .= $need_change_id3."\n";
 
-		$tagwriter->tag_data = $TagData;
+if ( $need_change_id3 = "yes" ) {		
+	// prepare tag
+	//$TaggingFormat = 'UTF-8';
+	$TaggingFormat = 'ISO-8859-1';
+	$getID3->setOption(array('encoding'=>$TaggingFormat));
+	// Initialize getID3 tag-writing module
+	require_once('../parts/get_id3/getid3/write.php');
+	$tagwriter = new getid3_writetags;
+	$tagwriter->filename       = $localtempfilename;
+	$tagwriter->tagformats     = array('id3v1', 'id3v2.3', 'ape');
+	// set various options (optional)
+	$tagwriter->overwrite_tags = true;
+	$tagwriter->tag_encoding   = $TaggingFormat;
+	$tagwriter->remove_other_tags = true;
 
-		// write tags
-		if ($tagwriter->WriteTags()) {
-			//echo 'Successfully wrote tags<br>';
-			if (!empty( $tagwriter->warnings )) {
-				echo 'There were some warnings:<br>'.implode('<br><br>', $tagwriter->warnings);
-			}
-		} else {
-			echo 'Failed to write tags!<br>'.implode('<br><br>', $tagwriter->errors);
+	// populate data array
+	$TagData['title'][]   = $title;
+	$TagData['artist'][]  = $artist;
+	$TagData['album'][]   = 'SRB - Das Buergerradio';
+	$TagData['year'][]    = date("Y");
+	$tagwriter->tag_data = $TagData;
+
+	// write tags v2
+	if ($tagwriter->WriteTags()) {
+		$log_message .= "write v2\n";
+		//echo 'Successfully wrote tags<br>';
+		if (!empty( $tagwriter->warnings )) {
+			echo 'There were some warnings:<br>'.implode('<br><br>', $tagwriter->warnings);
 		}
+	} else {
+		echo 'Failed to write tags V2!<br>'.implode('<br><br>', $tagwriter->errors);
+	}
+
+	//$TaggingFormat = 'ISO-8859-1';
+	//$getID3->setOption(array('encoding'=>$TaggingFormat));
+	//$tagwriter = new getid3_writetags;
+	//$tagwriter->filename       = $localtempfilename;
+	//$tagwriter->tagformats = array('id3v1');
+	//$tagwriter->tag_encoding   = $TaggingFormat;
+	//$TagData['album'][0] = replace_umlaute_sonderzeichen('SRB - Das Bürgerradio');
+	//$tagwriter->tag_data = $TagData;
+
+	// write tags v1
+	//if ($tagwriter->WriteTags()) {
+		//$log_message .= "write v1\n";
+		//echo 'Successfully wrote tags<br>';
+		//if (!empty( $tagwriter->warnings )) {
+			//echo 'There were some warnings:<br>'.implode('<br><br>', $tagwriter->warnings);
+		//}
+	//} else {
+		//echo 'Failed to write tags V1!<br>'.implode('<br><br>', $tagwriter->errors);
+	//}
 		
-	}// End change_id3
-		
-		
-		$need_change_mp3gain = "no";
-		
+}// End change_id3
+	
+	
+$need_change_mp3gain = "no";
+	
 		//if (@$ThisFileInfo['playtime_seconds'] < 600 ){
 		// mp3Gain pruefen wenn gewuenscht, nur wenn nicht zu lang!
 		if ( $set_mp3gain == "yes" ) {
@@ -111,42 +131,27 @@ if ( $fp_remote = fopen( $remotefilename, 'rb')) {
 		} //}// Ende mp3Gain wenn gewuenscht			
 
 		
-		if ( $need_change_id3 == "yes" or $need_change_mp3gain == "yes" ){	
-			// delete original file
-			unlink( $pathfilename );		
-			if ( !copy( $localtempfilename, $pathfilename )) {
-   			print ("failed to copy $pathfilename...<br>\n");
-			}else {
-				chmod($pathfilename, 0764 );
-				}}
-      // Delete temporary file
-      unlink( $localtempfilename );
+if ( $need_change_id3 == "yes" or $need_change_mp3gain == "yes" ) {	
+	// delete original file
+	unlink( $pathfilename );		
+	if ( !copy( $localtempfilename, $pathfilename )) {
+   	print ("failed to copy $pathfilename...<br>\n");
+	} else {
+		chmod($pathfilename, 0764 );
+	}
+}
 
+// Delete temporary file
+unlink( $localtempfilename );
 
-
-// Optional: copies data from all subarrays of [tags] into [comments] so
-// metadata is all available in one location for all tag formats
-// metainformation is always available under [tags] even if this is not called
-
-//getid3_lib::CopyTagsToComments($ThisFileInfo);
-
-// Output desired information in whatever format you want
-// Note: all entries in [comments] or [tags] are arrays of strings
-// See structure.txt for information on what information is available where
-// or check out the output of /demos/demo.browse.php for a particular file
-// to see the full detail of what information is returned where in the array
-//echo @$ThisFileInfo['comments_html']['artist'][0]."<br>"; // artist from any/all available tag formats
-//echo @$ThisFileInfo['tags']['id3v2']['title'][0]."<br>";  // title from ID3v2
-//echo @$ThisFileInfo['audio']['bitrate']."<br>";           // audio bitrate
-
- //write_log_file( $log_message, $TagData );
+//write_log_file( $log_message, $TagData );
 
 return @$ThisFileInfo['playtime_seconds']."<br>";            // playtime in minutes:seconds, formatted string
 //echo @$ThisFileInfo['error'][0]."<br>";
 }
 
 function write_log_file( $wert, $TagData ) {
-	// logfile schreiben
+	// write logfile
 	$myFile = "/tmp/admin_srb_audiofile_edit.log";
 	$fh = fopen($myFile, 'w+') or die("can't open file ".$myFile);
 	$stringData = $wert."\n";
