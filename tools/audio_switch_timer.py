7785fa3d9b6db2f4815488769d6cfe8afbcbe92f
srb-fm <technik@srb.fm>
2015-11-02 17:34:25 +0100

    added params from db

--- tools/audio_switch_timer.py
+++ tools/audio_switch_timer.py
@@ -48,19 +48,70 @@ class app_config(object):
     def __init__(self):
         """Settings"""
         # app_config
-        self.app_id = "023"
+        self.app_id = "025"
         self.app_desc = "Audio_Switch_Timer"
         self.app_errorfile = "error_audio_switch_timer.log"
+        # key for config in db
+        self.app_config = "audio_switch"
+        # amount parameter
+        self.app_config_params_range = 7
+        # params-type-list
+        self.app_params_type_list = []
+        self.app_params_type_list.append("p_string")
+        self.app_params_type_list.append("p_string")
+        self.app_params_type_list.append("p_string")
+        self.app_params_type_list.append("p_int")
+        self.app_params_type_list.append("p_int")
+        self.app_params_type_list.append("p_string")
+        self.app_params_type_list.append("p_int")
+        self.app_params_type_list.append("p_int")
         # errorlist
         self.app_errorslist = []
         self.app_errorslist.append("Parameter-Typ oder Inhalt stimmt nicht ")
+        self.app_errorslist.append("Fehler beim Lesen Parameter Times")
         self.app_errorslist.append("Fehler beim Fade ")
         # using develop-params
         self.app_develop = "no"
         # display debugmessages on console yes or no: "no"
         # for normal usage set to no!!!!!!
-        self.app_debug_mod = "yes"
+        self.app_debug_mod = "no"
         # settings serial port
+
+
+def load_extended_params():
+    """load extended params"""
+    ext_params_ok = True
+    # extern tools
+    ext_params_ok = lib_cm.params_provide_server_settings(ac, db)
+    if ext_params_ok is None:
+        return None
+    lib_cm.set_server(ac, db)
+
+    # Times
+    db.ac_config_times = db.params_load_1a(ac, db, "PO_Time_Config_1")
+    if db.ac_config_times is not None:
+        # Erweiterte Paramsliste anlegen
+        app_params_type_list_times = []
+        # Erweiterte Params-Type-List,
+        # Typ entsprechend der Params-Liste in der Config
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        app_params_type_list_times.append("p_string")
+        # Erweiterte Params pruefen
+        param_check_times = lib_cm.params_check_a(
+                        ac, db, 8,
+                        app_params_type_list_times,
+                        db.ac_config_times)
+        if param_check_times is None:
+            db.write_log_to_db_a(ac, ac.app_errorslist[2], "x",
+            "write_also_to_console")
+            return None
+    return ext_params_ok
 
 
 def load_switch_inputs():
@@ -102,7 +153,7 @@ def fade_switch(param):
     if switch_imput_old == switch_imput_new:
         log_message = ("Kein fade noetig, Input "
                                 + switch_imput_new + " bereits aktiv")
-        db.write_log_to_db_a(ac, log_message, "t", "write_also_to_console")
+        db.write_log_to_db_a(ac, log_message, "e", "write_also_to_console")
         return
     port = ser.set_port(ac, db)
     if not port:
@@ -110,7 +161,7 @@ def fade_switch(param):
     try:
         log_message = ("Fade from Input " + switch_imput_old
                         + " to Input " + switch_imput_new)
-        db.write_log_to_db_a(ac, log_message, "e", "write_also_to_console")
+        db.write_log_to_db_a(ac, log_message, "p", "write_also_to_console")
         # fade old input out
         x = 1
         for x in range(18):
@@ -137,10 +188,10 @@ def fade_switch(param):
         ser.reset_gain(ac, db, switch_imput_old)
         time.sleep(0.2)
         port.close
-        log_message = "Faded to Input " + switch_imput_new
+        log_message = ac.app_desc + " Faded to Input " + switch_imput_new
         db.write_log_to_db_a(ac, log_message, "i", "write_also_to_console")
     except Exception as e:
-        db.write_log_to_db_a(ac, ac.app_errorslist[2] + str(e), "x",
+        db.write_log_to_db_a(ac, ac.app_errorslist[3] + str(e), "x",
             "write_also_to_console")
         port.close
 
@@ -157,25 +208,27 @@ def lets_rock():
 
     #print datetime.datetime.now()
     if time_now.minute == 59:
-        print "m1"
-        if time_now.second == 10:
-            print "s10"
+        #print "m1"
+        if time_now.second == 15:
+            #print "s10"
             ac.switch_input_new = ac.switch_inputs[2][15:16]
             log_message = "Load Input for top of the hour"
             db.write_log_to_db_a(ac, log_message, "t", "write_also_to_console")
-    if time_now.minute == 04:
-        if time_now.second == 10:
+    #if time_now.minute == 04:
+    if time_now.minute == int(db.ac_config_times[4]) - 1:
+        if time_now.second == 15:
             ac.switch_input_new = ac.switch_inputs[2][17:18]
             log_message = "Load Input for Switchtime 2"
             db.write_log_to_db_a(ac, log_message, "t", "write_also_to_console")
-    if time_now.minute == 29:
-        if time_now.second == 10:
+    #if time_now.minute == 29:
+    if time_now.minute == int(db.ac_config_times[5]) - 1:
+        if time_now.second == 15:
             ac.switch_input_new = ac.switch_inputs[2][19:20]
             log_message = "Load Input for Switchtime 3"
             db.write_log_to_db_a(ac, log_message, "t", "write_also_to_console")
 
     if time_now.second == 35:
-        log_message = "Fade zu Input " + ac.switch_input_new + " vorgesehen"
+        log_message = "Input " + ac.switch_input_new + " vorgesehen"
         db.write_log_to_db_a(ac, log_message, "t", "write_also_to_console")
 
     if time_now.second == 59:
@@ -196,12 +249,40 @@ if __name__ == "__main__":
     log_message = ac.app_desc + " gestartet"
     db.write_log_to_db_a(ac, log_message, "r", "write_also_to_console")
     # losgehts
-    switch_status = ser.get_status(ac, db, "-s", "I")
-    if not switch_status:
-        log_message = ac.app_desc + " gestoppt"
-        db.write_log_to_db_a(ac, log_message, "s", "write_also_to_console")
-        sys.exit()
-    ac.time_forward = datetime.datetime.now() + datetime.timedelta(seconds=+ 65)
+    # Config_Params 1
+    db.ac_config_1 = db.params_load_1(ac, db)
+    if db.ac_config_1 is not None:
+        param_check = lib_cm.params_check_1(ac, db)
+         # alles ok: weiter
+        if param_check is not None:
+            # losgehts
+            # extendet params
+            load_extended_params_ok = load_extended_params()
+            if load_extended_params_ok is None:
+                log_message = ac.app_desc + " gestoppt"
+                db.write_log_to_db_a(ac, log_message, "s",
+                                            "write_also_to_console")
+                sys.exit()
+            # check if serial port is a port number or an string
+            if ac.server_active == "A":
+                if db.ac_config_1[1].isdigit():
+                    ac.ser_port = int(db.ac_config_1[1])
+                else:
+                    ac.ser_port = db.ac_config_1[1]
+            if ac.server_active == "B":
+                if db.ac_config_1[2].isdigit():
+                    ac.ser_port = int(db.ac_config_1[2])
+                else:
+                    ac.ser_port = db.ac_config_1[2]
+
+            switch_status = ser.get_status(ac, db, "-s", "I")
+            if not switch_status:
+                log_message = ac.app_desc + " gestoppt"
+                db.write_log_to_db_a(ac, log_message, "s",
+                                            "write_also_to_console")
+                sys.exit()
+    ac.time_forward = datetime.datetime.now() + datetime.timedelta(seconds=+ 70)
     ac.switch_inputs = load_switch_inputs()
+    db.write_log_to_db_a(ac, "Starting timer", "t", "write_also_to_console")
     lets_rock()
     sys.exit()
