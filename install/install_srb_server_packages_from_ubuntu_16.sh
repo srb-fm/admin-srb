9527b23cbd8dfd11427b99703ff846ecf5ecb53a
srb-fm <technik@srb.fm>
2016-05-25 11:24:39 +0200

    added ppa for mp3gain

---
+++ install/install_srb_server_packages_from_ubuntu_16.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# This script is for installing server-packages.
+# Some of them must be configured later.
+# Testet with ubuntu-server 14.04 in 2015
+#
+# Dieses kleine Script uebernimmt die Instalaltion von Paketen
+# fuer den srb-admin-server
+# Bei einigen ist eine spaetere Configuration noetig
+# Author: Joerg Sorge
+# Distributed under the terms of GNU GPL version 2 or later
+# Copyright (C) Joerg Sorge joergsorge@gmail.com
+# 2012-09-17
+
+echo "Admin-SRB-Server Packageinstall prepaere..."
+
+echo "PPA for mp3gain ..."
+sudo add-apt-repository ppa:flexiondotorg/audio
+sudo apt-get update
+
+echo "Admin-SRB-Server Packageinstall..."
+
+# TODO: php7!?
+
+sudo apt-get install \
+git ntp php5-cli php5-dev php-pear \
+phpmyadmin vsftpd mpc \
+python-tk python-mutagen python-setuptools \
+lame mp3val libid3-tools mp3gain sox mp3info \
+libsox-fmt-all darkice id3v2 \
+curl gawk links libtranslate-bin
+
+sudo pear install HTTP_Download
+
+echo "Load ez_setup.py.."
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
+
+echo "Install pip"
+sudo easy_install pip
+
+echo "Load Tweepy from git ..."
+git clone https://github.com/tweepy/tweepy.git
+
+echo "Install Tweepy"
+cd "$(pwd)"/tweepy
+sudo python setup.py install
+
+echo "Clean up Tweepy-Temp"
+sudo rm -R "$(pwd)"/tweepy
+sudo rm "$(pwd)"/setuptools-14.0.zip
+
+echo "...finish"
+exit
