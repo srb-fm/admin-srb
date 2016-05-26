8ee57c55fb749c91064f2b44cace15a8e6b7a2a4
srb-fm <technik@srb.fm>
2016-05-26 18:20:17 +0200

    changed to css v3

--- intra/admin_srb_inventar/iv_vl_find.php
+++ intra/admin_srb_inventar/iv_vl_find.php
@@ -33,7 +33,7 @@ if ( isset( $_GET['vl_kat'] ) ) {
 <head>
 	<title>Admin-SRB-Inventar finden</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
-	<style type="text/css">	@import url("../parts/style/style_srb_2.css");   </style>
+	<style type="text/css">	@import url("../parts/style/style_srb_3.css");   </style>
 	<style type="text/css"> @import url("../parts/style/style_srb_jq_2.css");    </style>
 	<style type="text/css"> @import url("../parts/jquery/jquery_ui_1_8_16/css/jquery-ui-1.8.16.custom.css");    </style>
 	<script src="../parts/jquery/jquery_1_7_1/jquery.min.js"></script>
