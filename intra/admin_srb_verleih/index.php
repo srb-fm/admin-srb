4297f99a64fbd6ba9f8aea6d619c4f1731dcf507
srb-fm <technik@srb.fm>
2016-05-27 16:31:10 +0200

    changed to style 3

--- intra/admin_srb_verleih/index.php
+++ intra/admin_srb_verleih/index.php
@@ -20,7 +20,7 @@ require "../../cgi-bin/admin_srb_libs/lib_sess.php";
 <head>
 	<title>Admin-SRB-Verleih</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" >
-	<style type="text/css">	@import url("../parts/style/style_srb_2.css");    </style>
+	<style type="text/css">	@import url("../parts/style/style_srb_3.css");    </style>
 	<style type="text/css">	@import url("../parts/style/style_srb_jq_2.css");    </style>
 	<style type="text/css"> @import url("../parts/colorbox/colorbox.css");  </style>
 	
