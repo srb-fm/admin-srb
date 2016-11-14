d893107bf473a62c0eb13b6538ae27828a66cb79
SRB <technik@srb.fm>
2016-11-14 21:38:57 +0100

    Added ppa for firebird

--- install/install_srb_server_firebird.sh
+++ install/install_srb_server_firebird.sh
@@ -19,14 +19,17 @@ echo "Use this script only for a fresh firebird-install!"
 read -p "Are you sure to install? (y/n) " -n 1
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "Adding the ppa"
+	sudo add-apt-repository ppa:mapopa
+	sudo apt-get update
 	echo "Install..."
 	sudo apt-get install \
 	firebird2.5-super firebird2.5-examples firebird2.5-dev \ 
-	php5-interbase python-kinterbasdb
+	php5-interbase libapache2-mod-php5 python-kinterbasdb
 
 	echo "Configuration"
 	sudo dpkg-reconfigure firebird2.5-super
-
+	sudo php5enmod interbase
 	echo "Restart Apache ..."
 	sudo service apache2 restart
 	echo "...finish"
