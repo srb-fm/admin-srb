f29b4079a5d5ed6278777a40432ae0796a6f1c50
SRB <technik@srb.fm>
2016-11-21 21:27:13 +0100

    change behavior to add or replace alias

--- install/config_firebird.sh
+++ install/config_firebird.sh
@@ -104,14 +104,19 @@ else
 	echo "Customize Aliases..."
 	if [ $db_option == "create" ]; then
 		sudo cp /etc/firebird/2.5/aliases.conf /etc/firebird/2.5/aliases.conf.$(date +'%y-%m-%d-%H-%M-%S')
-		text="Admin_SRB_db = /var/lib/firebird/2.5/data/admin_srb_db.fdb"
-		if ! grep -Fxn "$text" /etc/firebird/2.5/aliases.conf /etc/firebird/2.5/aliases.conf; then
-			sudo bash -c "echo ""Admin_SRB_db = /var/lib/firebird/2.5/data/admin_srb_db.fdb"" >> /etc/firebird/2.5/aliases.conf"
+		text="Admin_SRB_db"
+		if grep -Fn "$text" /etc/firebird/2.5/aliases.conf /etc/firebird/2.5/aliases.conf; then
+			line_nr=$(grep -Fn "$text" /etc/firebird/2.5/aliases.conf | sed 's/^\([0-9]\+\):.*$/\1/')
+			sed -i "${line_nr}d" /etc/firebird/2.5/aliases.conf
 		fi
-		text="Admin_SRB_db_log = /var/lib/firebird/2.5/data/admin_srb_db_log.fdb"
-		if ! grep -Fxn "$text" /etc/firebird/2.5/aliases.conf /etc/firebird/2.5/aliases.conf; then
-			sudo bash -c "echo ""Admin_SRB_db_log = /var/lib/firebird/2.5/data/admin_srb_db_log.fdb"" >> /etc/firebird/2.5/aliases.conf"
+		sudo bash -c "echo ""Admin_SRB_db = /var/lib/firebird/2.5/data/admin_srb_db.fdb"" >> /etc/firebird/2.5/aliases.conf"
+		
+		text="Admin_SRB_db_log"
+		if grep -Fn "$text" /etc/firebird/2.5/aliases.conf /etc/firebird/2.5/aliases.conf; then
+			line_nr=$(grep -Fn "$text" /etc/firebird/2.5/aliases.conf | sed 's/^\([0-9]\+\):.*$/\1/')
+			sed -i "${line_nr}d" /etc/firebird/2.5/aliases.conf
 		fi
+		sudo bash -c "echo ""Admin_SRB_db_log = /var/lib/firebird/2.5/data/admin_srb_db_log.fdb"" >> /etc/firebird/2.5/aliases.conf"
 	fi
 fi
 
