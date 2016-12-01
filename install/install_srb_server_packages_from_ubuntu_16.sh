ca5efa0437e1fd19eb6386a12e8ecf6bc4125c35
SRB <technik@srb.fm>
2016-12-01 21:15:45 +0100

    Added ubunt 16 specific packages

--- install/install_srb_server_packages_from_ubuntu_16.sh
+++ install/install_srb_server_packages_from_ubuntu_16.sh
@@ -10,44 +10,52 @@
 # Author: Joerg Sorge
 # Distributed under the terms of GNU GPL version 2 or later
 # Copyright (C) Joerg Sorge joergsorge@gmail.com
-# 2012-09-17
-
+# 2016-05-17
+echo ""
 echo "Admin-SRB-Server Packageinstall prepaere..."
-
+echo ""
 echo "PPA for mp3gain ..."
 sudo add-apt-repository ppa:flexiondotorg/audio
-sudo apt-get update
+sudo apt update
 
+echo ""
 echo "Admin-SRB-Server Packageinstall..."
 
 # TODO: php7!?
 
-sudo apt-get install \
-git ntp php5-cli php5-dev php-pear \
+sudo apt install \
+git ntp php-cli php-dev php-pear \
 phpmyadmin vsftpd mpc \
-python-tk python-mutagen python-setuptools \
+python python-tk python-mutagen python-setuptools \
 lame mp3val libid3-tools mp3gain sox mp3info \
 libsox-fmt-all darkice id3v2 \
 curl gawk links libtranslate-bin
 
 sudo pear install HTTP_Download
 
-echo "Load ez_setup.py.."
+echo ""
+echo "Load ez_setup.py..."
 wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python
 
-echo "Install pip"
+echo ""
+echo "Install pip..."
 sudo easy_install pip
 
+echo ""
 echo "Load Tweepy from git ..."
 git clone https://github.com/tweepy/tweepy.git
 
-echo "Install Tweepy"
+echo ""
+echo "Install Tweepy..."
 cd "$(pwd)"/tweepy
 sudo python setup.py install
+cd ..
 
-echo "Clean up Tweepy-Temp"
+echo ""
+echo "Clean up Tweepy-Temp..."
 sudo rm -R "$(pwd)"/tweepy
-sudo rm "$(pwd)"/setuptools-14.0.zip
+sudo rm "$(pwd)"/setuptools*.zip
 
 echo "...finish"
+echo ""
 exit
