d9eee7b7fd39d843ed5df0206587b360193bf3ce
SRB <technik@srb.fm>
2016-12-02 11:36:58 +0100

    polish

--- install/04_config_paths.sh
+++ install/04_config_paths.sh
@@ -2,7 +2,7 @@
 
 # This script is for configuration paths.
 #
-# Testet with ubuntu-server 14.04 in 2016
+# Testet with ubuntu-server 14.04 and 16.04 in 2016
 #
 # Dieses kleine Script uebernimmt die Configuration
 # der Pfade fuer Admin-SRB auf dem Server
@@ -12,6 +12,7 @@
 # Copyright (C) Joerg Sorge joergsorge@gmail.com
 # 2016-11-29
 #
+echo ""
 echo "Admin-SRB Data Path Configuration..."
 echo "Use this script only for a fresh install!"
 echo "Run this script not with sudo!"
@@ -20,6 +21,7 @@ echo "It provides following steps:"
 echo "- Set permissions for main path"
 echo "- Making sub paths"
 
+echo ""
 read -p "Are you sure to config file-paths? (y/n) " -n 1
 echo ""
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -31,6 +33,7 @@ fi
 echo "The standard path is: /mnt/data_server"
 echo "Leave the input blank if you using this path"
 echo "Otherwise type in a new path"
+echo ""
 read -p 'input data path for media files: ' path_media
 if [ -z "$path_media" ]; then
   path_media=/mnt/data_server
@@ -55,5 +58,5 @@ mkdir $path_media/Play_Out_Server/Play_Out_Sendung
 echo "Congratiulations!"
 echo "Now you can take off your shoes and sitting down in a chair..."
 echo "...finish"
-
+echo ""
 exit
