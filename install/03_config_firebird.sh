9ac7dd92bccc8373a336f7e41b094a9535bef63c
SRB <technik@srb.fm>
2016-12-02 11:37:57 +0100

    added info

--- install/03_config_firebird.sh
+++ install/03_config_firebird.sh
@@ -2,7 +2,7 @@
 
 # This script is for configuration firebird-sql-server.
 #
-# Testet with ubuntu-server 14.04 in 2016
+# Testet with ubuntu-server 14.04 and 16.04 in 2016
 #
 # Dieses kleine Script uebernimmt die Configuration
 # des Firebird-Servers Admin-SRB auf dem Server
